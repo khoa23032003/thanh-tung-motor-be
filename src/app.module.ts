03f323d7dcd96ece9f3564b1d2378be6c4f77ba4
khoa23032003 <2minhkhoa23032003@gmail.com>
2026-04-13 15:07:56 +0700

    get brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { BrandModule } from './brand/brand.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    BrandModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
